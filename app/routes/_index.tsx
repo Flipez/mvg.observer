0a81af8b08cbc3604b5966a5edeca49dd781d5d2
Christopher Stöckl <christopher@cstoeckl.de>
2025-02-12 23:28:22 +0100

    Refactoring

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,53 +2,52 @@ import { useEffect, useState } from "react"
 import type { MetaFunction } from "@remix-run/node"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs"
 import { DepartureGrid } from "~/departures/grid"
+import { SubwayMap } from "~/departures/map"
+import type { Departure, StationState } from "~/types/departures"
 import { EventSource } from "eventsource"
+
 import { columns } from "../departures/table/columns"
 import { DataTable } from "../departures/table/data-table"
-import { SubwayMap } from "~/departures/map";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "MVG Live" },
-    { name: "description", content: "Live view of MVG Subway metrics" },
-  ]
-}
+export const meta: MetaFunction = () => [
+  { title: "MVG Live" },
+  { name: "description", content: "Live view of MVG Subway metrics" },
+]
 
 export default function Index() {
-  const [departures, setDepartures] = useState({})
+  const [departures, setDepartures] = useState<StationState>({})
 
   useEffect(() => {
     const sse = new EventSource("https://live.mvg.auch.cool/events")
 
-    sse.onmessage = function (event) {
-      var payload = JSON.parse(event.data)
+    sse.onmessage = (event) => {
+      const payload = JSON.parse(event.data)
+      const { departures, station } = payload
 
-      var avgDelay =
-        payload["departures"].reduce(
-          (accumulator: number, currentValue: { delayInMinutes: number }) =>
-            accumulator + currentValue.delayInMinutes,
-          0
-        ) / payload["departures"].length
-      payload["avgDelay"] = Math.round(avgDelay * 100) / 100
+      const totalDelay = departures.reduce(
+        (acc: number, curr: Departure) => acc + curr.delayInMinutes,
+        0
+      )
+      const avgDelay = Math.round((totalDelay / departures.length) * 100) / 100
 
-      setDepartures((prevDepartures) => ({
-        ...prevDepartures, // Copy previous state
-        [payload["station"]]: payload, // Update specific station
+      setDepartures((prev) => ({
+        ...prev,
+        [station]: { ...payload, avgDelay },
       }))
 
-      const grid = document.getElementById(payload["station"])
-      grid?.classList.add("animate-ping")
-      setTimeout(() => grid?.classList.remove("animate-ping"), 25)
+      const grid = document.getElementById(station)
+      if (grid) {
+        grid.classList.add("animate-ping")
+        setTimeout(() => grid.classList.remove("animate-ping"), 25)
+      }
     }
 
-    return () => {
-      sse.close()
-    }
+    return () => sse.close()
   }, [])
 
   return (
     <div className="container mx-auto">
-      <Tabs defaultValue="grid" className="">
+      <Tabs defaultValue="grid">
         <TabsList className="grid w-full grid-cols-3">
           <TabsTrigger value="grid">Grid</TabsTrigger>
           <TabsTrigger value="table">Table</TabsTrigger>
